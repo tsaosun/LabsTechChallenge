0ceaa23ae4730183fbfee1b764ccceaf05d0ca8f
tsaosun <ts2931@columbia.edu>
2018-01-17 23:36:19 -0500

    Made appropriate changes for /ts2931 directory

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/ts2931', methods=['GET'])
+def tahsina():
+    return render_template('tahsina.html')
 
 if __name__ == '__main__':
+    app.run()
+
+if __name__ == '__tahsina__':
     app.run()
